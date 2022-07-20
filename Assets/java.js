984caa6f643bf033c507cbc2fde74541b5d874c9
ahanna2 <103732152+ahanna2@users.noreply.github.com>
2022-07-19 19:22:51 -0500

    adding Ajax for Cars api

---
+++ Assets/java.js
@@ -0,0 +1,15 @@
+// Ajax for Cars Api
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://car-data.p.rapidapi.com/cars/types",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "dc01b57ea1mshaa4f3952e2dd924p15fbecjsn41cb7f5ff069",
+		"X-RapidAPI-Host": "car-data.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
