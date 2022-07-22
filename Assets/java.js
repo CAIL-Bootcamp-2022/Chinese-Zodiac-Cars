c752e9b9e6c8a09dd653c7d7ffc956edcdb9e006
ahanna2 <103732152+ahanna2@users.noreply.github.com>
2022-07-21 19:21:53 -0500

    test

--- Assets/java.js
+++ Assets/java.js
@@ -1,15 +1,21 @@
 // Ajax for Cars Api
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://car-data.p.rapidapi.com/cars/types",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "dc01b57ea1mshaa4f3952e2dd924p15fbecjsn41cb7f5ff069",
-		"X-RapidAPI-Host": "car-data.p.rapidapi.com"
-	}
-};
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://car-data.p.rapidapi.com/cars/types",
+// 	"method": "GET",
+// 	"headers": {
+// 		"X-RapidAPI-Key": "dc01b57ea1mshaa4f3952e2dd924p15fbecjsn41cb7f5ff069",
+// 		"X-RapidAPI-Host": "car-data.p.rapidapi.com"
+// 	}
+// };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+document.getElementById('WheelButton').addEventListener('click', loadText);
+
+function loadText(){
+console.log('test')
+}
